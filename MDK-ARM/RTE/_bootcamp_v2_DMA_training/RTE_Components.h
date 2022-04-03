5e76f5b8337b0aa037287afb036a27f88e6682e6
nctiA <ncti.akyl00@gmail.com>
2022-04-04 01:24:13 +0300

    STM32
    STM32

---
+++ MDK-ARM/RTE/_bootcamp_v2_DMA_training/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'bootcamp_v2_DMA_training' 
+ * Target:  'bootcamp_v2_DMA_training' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
